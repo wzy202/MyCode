688d0f10b32a2fcccca6cc04fc14b4471022a76d
wzy <2756949672@qq.com>
2023-04-12 23:56:56 +0800

    创建项目

---
+++ study-projct-backend/src/main/java/com/wzy/studyprojctbackend/StudyProjctBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wzy.studyprojctbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyProjctBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyProjctBackendApplication.class, args);
+    }
+
+}
---
+++ study-projct-backend/src/test/java/com/wzy/studyprojctbackend/StudyProjctBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wzy.studyprojctbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyProjctBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
